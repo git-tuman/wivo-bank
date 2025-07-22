45cbb0406cd3b6ef3c6ab01ca3e83a61464c9644
Kirill <kirillvershov5500@gmail.com>
2025-07-22 21:56:13 +0300

    add hero content

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,16 +1,5 @@
-import {
-  COLOR,
-  FIND_OUT_MORE,
-  GET_STARTED,
-  HERO,
-  HERO_PARAGRAPH,
-  HERO_PARAGRAPH_MOB,
-  MAIN_HEADING,
-  MOB,
-  SIZE,
-} from "../constants";
+import { HERO, MOB } from "../constants";
 import useIsMobile from "../hooks/useIsMobile";
-import { Btn, BtnPrimary } from "../types";
 import Header from "./header/Header";
 import HeroBg from "./backgrounds/HeroBg";
 import BurgerMenu from "./header/BurgerMenu";
